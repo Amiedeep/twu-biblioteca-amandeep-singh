ce04e0e9aa5a450a3d6f59047165342d21177a01
Amandeep Singh <amandees@thoughtworks.com>
2015-08-16 22:15:03 +0530

    [Release-1] adding welcome message to the console

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,22 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    String welcomeMessage;
+
+    public BibliotecaApp() {
+        this.welcomeMessage = "Welcome to Bibliotecs App!";
+        printWelcomeMessage();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    private void printWelcomeMessage() {
+        System.out.println(getWelcomeMessage());
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,24 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ExampleTest {
 
+    BibliotecaApp testObject;
+
+    @Before
+    public void setUp() {
+        testObject = new BibliotecaApp();
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void getWelcomeMessageTest() {
+        assertThat(testObject.getWelcomeMessage(), is("Welcome to Bibliotecs App!"));
     }
 }
