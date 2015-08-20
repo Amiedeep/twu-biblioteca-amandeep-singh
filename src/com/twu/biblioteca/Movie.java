ac6a4a9616af1b95f7471ab680140b65111f38e9
Amandeep Singh <amandees@thoughtworks.com>
2015-08-20 16:36:10 +0530

    [Release 2]Adding class movie

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by amandeepsingh on 20/08/15.
+ */
+public class Movie {
+    private String name;
+    private String Year;
+    private float rating;
+    private String director;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+}
